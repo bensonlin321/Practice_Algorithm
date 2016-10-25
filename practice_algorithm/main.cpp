bdc5d8bb9f1bf96210387f71df2ea06457109fdf
Benson <benson@bensonde-macbook-pro.local>
2016-10-25 11:05:34 +0800

    Initial Commit

---
+++ practice_algorithm/main.cpp
@@ -0,0 +1,15 @@
+//
+//  main.cpp
+//  practice_algorithm
+//
+//  Created by Benson on 2016/10/25.
+//  Copyright © 2016年 Benson. All rights reserved.
+//
+
+#include <iostream>
+
+int main(int argc, const char * argv[]) {
+    // insert code here...
+    std::cout << "Hello, World!\n";
+    return 0;
+}
